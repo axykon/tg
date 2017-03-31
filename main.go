1956c3175eaacfc20e2e823ec68d11152c30e82f
Alexey Kravtsov <axykon@gmail.com>
2017-03-31 07:42:40 +0300

    Render title

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 )
 
 const (
-	windowTitle  = "Tank & Gun"
+	windowTitle  = "TANK & GUN"
 	windowWidth  = 800
 	windowHeight = 600
 )
 
+var (
+	window   *sdl.Window
+	renderer *sdl.Renderer
+)
+
 func main() {
-	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		log.Fatalf("Could not initialize SDL: %v\n", err)
 	}
 	defer sdl.Quit()
@@ -23,39 +28,49 @@ func main() {
 		log.Fatalf("Could not initialize SDL_ttf: %v\n", err)
 	}
 
-	window, err := sdl.CreateWindow(windowTitle,
-		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		windowWidth, windowHeight,
-		sdl.WINDOW_SHOWN)
+	window, renderer, err := sdl.CreateWindowAndRenderer(windowWidth, windowHeight, sdl.WINDOW_SHOWN)
 	if err != nil {
 		log.Fatalf("Could not create window: %v\n", err)
 	}
+	// defer renderer.Destroy()
 	defer window.Destroy()
 
-	font, err := ttf.OpenFont("res/ptsansb.ttf", 32)
+	font, err := ttf.OpenFont("res/ptsansb.ttf", 40)
 	if err != nil {
 		log.Fatalf("Could not open font: %v\n", err)
 	}
-	defer font.Close()
+	//defer font.Close()
+	defer ttf.Quit()
 
-	solid, err := font.RenderUTF8_Solid(windowTitle, sdl.Color{A: 255, R: 0, G: 244, B: 255})
+	message, err := font.RenderUTF8_Solid(windowTitle, sdl.Color{A: 255, R: 255, G: 255, B: 255})
 	if err != nil {
 		log.Fatalf("Could not render text: %v\n", err)
 	}
-	defer solid.Free()
+	defer message.Free()
 
 	surface, err := window.GetSurface()
 	if err != nil {
 		log.Fatalf("Could not get window surface: %v\n", err)
 	}
+	texture, err := renderer.CreateTextureFromSurface(message)
+	if err != nil {
+		log.Fatalf("Could not create texture: %v\n", err)
+	}
 	defer surface.Free()
+	defer texture.Destroy()
 
-	if err := solid.Blit(nil, surface, nil); err != nil {
-		log.Fatalf("Could not put text onto surface: %v\n", err)
+	renderer.SetDrawColor(255, 125, 0, 255)
+
+	if err := renderer.Clear(); err != nil {
+		log.Fatalf("Could not clear rendering target: %v", err)
 	}
-	if err := window.UpdateSurface(); err != nil {
-		log.Fatalf("Could not update window surface: %v\n", err)
-	}
+	renderer.Copy(texture, nil, &sdl.Rect{X: 10, Y: windowHeight / 4, W: windowWidth - 20, H: windowHeight / 2})
+	// renderer.CopyEx(texture, nil, nil, 0, &sdl.Point{100, 100}, sdl.FLIP_NONE)
+	renderer.Present()
+
+	// if err := window.UpdateSurface(); err != nil {
+	// 	log.Fatalf("Could not update window surface: %v\n", err)
+	// }
 
 loop:
 	for {
@@ -66,8 +81,6 @@ loop:
 			if event.Keysym.Sym == sdl.K_ESCAPE {
 				break loop
 			}
-		default:
-
 		}
 	}
 }
